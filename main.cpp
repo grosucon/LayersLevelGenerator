74fb45cbfe58ccbb28d447d1627841116c15a08f
Grosu Constantin <c.grosu95@gmail.com>
2017-03-02 17:15:54 +0100

    Better generate

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include <ctime>
 #include <iostream>
 
-//Hello from Russia
-
-#define COLORS_COUNT 5
+#define COLORS_COUNT 5 //colors numbers 0-4
 #define LAYER_HEIGHT 10
 #define LAYER_WIDTH 6
-#define forlayer for (int i=0; i<LAYER_HEIGHT; i++) for (int j=0; j<LAYER_WIDTH; j++)
+#define START 5
+#define FINISH 6
+#define BLOCK 7
 
 using namespace std;
 
@@ -16,12 +16,25 @@ int main() {
     int layer[LAYER_HEIGHT][LAYER_WIDTH];
 
     srand(time(NULL));
-    forlayer {
-            layer[i][j] = rand() % COLORS_COUNT + 1;
-            cout << layer[i][j] << flush;
-            if ( j == LAYER_WIDTH-1 ) cout << endl;
-            else cout << " " << flush;
-        };
+    for (int i=0; i<LAYER_HEIGHT; i++) {
+        for (int j = 0; j < LAYER_WIDTH; j++) {
+            layer[i][j] = rand() % COLORS_COUNT;
+            if ( j != 0 && rand() % 2 && rand() % 2 ) layer[i][j]=layer[i][j-1];
+            if ( i != 0 && rand() % 2 && rand() % 2 ) layer[i][j]=layer[i-1][j];
+        }
+    }
+
+    layer[LAYER_HEIGHT-1][rand() % LAYER_WIDTH]=START; //rand start and
+    layer[0][rand() % LAYER_WIDTH]=FINISH;             //final cell
+
+    for (int i=0; i<LAYER_HEIGHT; i++) {
+        for (int j = 0; j < LAYER_WIDTH; j++) {
+            cout << layer[i][j];
+            if (j == LAYER_WIDTH - 1) cout << endl;
+            else cout << " ";
+        }
+    }
+
 
     return 0;
 }
